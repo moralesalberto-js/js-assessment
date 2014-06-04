6ee39d21574eb69ab05385136cfab81175dbe8a7
moralesalberto <alberto@moralitos.com>
2014-06-04 08:34:28 -0400

    starting to work on the answers

--- app/arrays.js
+++ app/arrays.js
@@ -3,11 +3,22 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     indexOf : function(arr, item) {
-
+      var index = -1;
+      for(var i=0 ; i < arr.length ; i++) {
+        if (arr[i] === item) {
+          index = i;
+          break;
+        } 
+      }
+      return index;
     },
 
     sum : function(arr) {
-
+      var total = 0;
+      for(var i = 0; i < arr.length; i++) {
+        total += arr[i]; 
+      }
+      return total;
     },
 
     remove : function(arr, item) {
